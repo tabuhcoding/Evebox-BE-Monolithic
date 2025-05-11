b2d01ad12821ba9a3c4ae4374fab6db557e0ce97
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-05-11 16:25:38 +0700

    rm unused now

--- src/shared/utils/otp/otp.util.ts
+++ src/shared/utils/otp/otp.util.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { OTPType } from 'src/modules/user/domain/enums/otp-type.enum';
+// import { OTPType } from 'src/modules/user/domain/enums/otp-type.enum';
 import { v4 } from 'uuid';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
@@ -8,29 +8,29 @@ import { ConfigService } from '@nestjs/config';
 export class OtpUtils {
   constructor(private readonly configService: ConfigService) {}
 
-  generateToken(email: string, type: OTPType): string {
-    const timestamp = Date.now();
-    const randomString = v4();
-    return `${email}_${type}_${timestamp}_${randomString}`;
-  }
+  // generateToken(email: string, type: OTPType): string {
+  //   const timestamp = Date.now();
+  //   const randomString = v4();
+  //   return `${email}_${type}_${timestamp}_${randomString}`;
+  // }
 
-  generateRequestToken(email: string, type: string): string {
-    const uniqueIdentifier = Buffer.from(`${email}:${type}`).toString('base64');
+  // generateRequestToken(email: string, type: string): string {
+  //   const uniqueIdentifier = Buffer.from(`${email}:${type}`).toString('base64');
 
-    return jwt.sign(
-      {
-        kind: 'verify_email',
-        payload: {
-          email,
-          type,
-        },
-        jti: uniqueIdentifier, // Add as JWT ID
-      },
-      this.configService.get('JWT_SECRET'),
-      {
-        expiresIn: '1h',
-        noTimestamp: true, // Remove iat claim
-      },
-    );
-  }
+  //   return jwt.sign(
+  //     {
+  //       kind: 'verify_email',
+  //       payload: {
+  //         email,
+  //         type,
+  //       },
+  //       jti: uniqueIdentifier, // Add as JWT ID
+  //     },
+  //     this.configService.get('JWT_SECRET'),
+  //     {
+  //       expiresIn: '1h',
+  //       noTimestamp: true, // Remove iat claim
+  //     },
+  //   );
+  // }
 }

2ac823e8f1b49fb88775aabd67f86f318b1d4625
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-05-11 15:34:31 +0700

    init repo

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
+import { PrismaModule } from './infrastructure/database/prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService globally available
+      envFilePath: '.env', // Path to .env file
+    }),
+    PrismaModule,
+    CqrsModule,
+
+  ],
+})
+export class AppModule {}

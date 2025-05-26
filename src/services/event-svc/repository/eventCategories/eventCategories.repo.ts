359e8b080157d356d199a1648e49a042810222d9
DatTruong0108 <dattantruong.work@gmail.com>
2025-05-26 10:41:27 +0700

    feat: create & update event cat

--- src/services/event-svc/repository/eventCategories/eventCategories.repo.ts
+++ src/services/event-svc/repository/eventCategories/eventCategories.repo.ts
@@ -1,10 +1,13 @@
 import { BaseRepository } from "src/shared/repo/base.repository";
 import { EventCategories } from "@prisma/client";
 import { Prisma } from "@prisma/client";
+import { Result } from "oxide.ts";
 
 export { EventCategories } from "@prisma/client";
 
 export interface EventCategoriesRepository
   extends BaseRepository<EventCategories, Prisma.EventCategoriesDelegate> {
   // Thêm các method riêng cho EventCategories nếu cần, ví dụ:
+  createEventCategory(eventId: number, categoryIds: number[]): Promise<Result<any, Error>>;
+  updateEventCategory(eventId: number, categoryIds: number[]): Promise<Result<any, Error>>;
 }

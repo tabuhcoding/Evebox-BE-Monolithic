8af169ea7f1f3b96788a9f7a814ec9508832b851
DatTruong0108 <dattantruong.work@gmail.com>
2025-05-29 23:15:27 +0700

    fix: email in request of change-pw api

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const options = new DocumentBuilder()
     .setTitle('Evebox API')
     .setDescription('The Evebox API description')
-    .setVersion('1.0')
+    .setVersion('1.1')
     .addBearerAuth(
       {
          type: 'http',

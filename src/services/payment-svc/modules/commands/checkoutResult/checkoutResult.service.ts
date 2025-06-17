58ce8fa7e0e954619902a17006c8b5d4ea8dbbd9
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-06-17 23:20:11 +0700

    fix readingMap

--- src/services/payment-svc/modules/commands/checkoutResult/checkoutResult.service.ts
+++ src/services/payment-svc/modules/commands/checkoutResult/checkoutResult.service.ts
@@ -50,8 +50,8 @@ export class CheckoutResultService {
       // TODO: Need to run in transaction
       // Update seat status
       {
-        const seatIDs = cachedData.data[0].ticketTypeSelection.map(ticket => ticket.seatInfo.map(seat => seat.seatId)).flat();
-        await this.getTicketTypeDetailService.setSeatStatusToESold(cachedData.data[0].showingId, seatIDs);
+        const seatIDs = cachedData.data[0].ticketTypeSelection?.map(ticket => ticket.seatInfo?.map(seat => seat.seatId)).flat();
+        if ( seatIDs.length > 0 )await this.getTicketTypeDetailService.setSeatStatusToESold(cachedData.data[0].showingId, seatIDs);
       }
       // Generate the ticket
       const seatmapType = await this.generateTicketService.execute(webhookData.orderCode, cachedData.data[0].ticketTypeSelection)

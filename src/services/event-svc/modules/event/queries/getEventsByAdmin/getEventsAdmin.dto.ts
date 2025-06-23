d11d8cd6081711819755a8e4bad4435243254d19
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-06-23 23:08:55 +0700

    fix get event admin

---
+++ src/services/event-svc/modules/event/queries/getEventsByAdmin/getEventsAdmin.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { PaginationQuery } from "src/shared/constants/pagination";
+
+export class GetEventsAdminDto extends PaginationQuery {
+  @ApiProperty({
+    example: 'Kịch idecaf',
+    description: 'Filter events by title',
+    required: false,
+  })
+  title?: string;
+
+  @ApiProperty({
+    example: true,
+    description: 'Filter events by approval status',
+    required: false,
+    type: Boolean,
+  })
+  isApproved?: boolean;
+
+  @ApiProperty({
+    example: true,
+    description: 'Filter events by deletion status',
+    required: false,
+    type: Boolean,
+  })
+  isDeleted?: boolean;
+
+  @ApiProperty({
+    example: '2024-12-28T13:00:00.000Z',
+    description: 'Filter events created after this date',
+    required: false,
+    type: String,
+  })
+  createdFrom?: string;
+
+  @ApiProperty({
+    example: '2024-12-28T13:00:00.000Z',
+    description: 'Filter events created before this date',
+    required: false,
+    type: String,
+  })
+  createdTo?: string;
+}
+  

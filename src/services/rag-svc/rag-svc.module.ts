484380c6242911d89acb1721c8969e32f869545e
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-06-23 21:46:42 +0700

    openAImodule

--- src/services/rag-svc/rag-svc.module.ts
+++ src/services/rag-svc/rag-svc.module.ts
@@ -3,9 +3,10 @@ import { ContentController } from "./modules/content/content.controller";
 import { ContentService } from "./modules/content/content.service";
 import { ContentRepositoryImpl } from "./repository/content.impl";
 import { PrismaAIModule } from "./database/prisma-ai/prisma.module";
+import { OpenAIModule } from "./modules/openai/openAI.module";
 
 @Module({
-  imports: [ PrismaAIModule ],
+  imports: [ PrismaAIModule, OpenAIModule ],
   controllers: [ContentController],
   providers: [
     ContentService,

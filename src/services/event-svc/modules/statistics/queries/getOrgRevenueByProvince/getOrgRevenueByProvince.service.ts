5f6f657d76b3d5c842d526102451f4f9f124163a
tabuhcoding <baobao11062003@gmail.com>
2025-07-12 01:16:37 +0700

    add cache

--- src/services/event-svc/modules/statistics/queries/getOrgRevenueByProvince/getOrgRevenueByProvince.service.ts
+++ src/services/event-svc/modules/statistics/queries/getOrgRevenueByProvince/getOrgRevenueByProvince.service.ts
@@ -4,6 +4,7 @@ import { ProvinceRevenueData } from "./getOrgRevenueByProvince-response.dto";
 import { EventsRepository } from "src/services/event-svc/repository/events/events.repo";
 import { GetAdminAccessService } from "src/services/auth-svc/modules/user/queries/get-admin-access/get-admin-access.service";
 import { SlackService } from "src/infrastructure/adapters/slack/slack.service";
+import { FileCacheService } from "src/infrastructure/cache/fileCache/fileCache.service";
 
 @Injectable()
 export class GetOrgRevenueByProvinceService {
@@ -11,10 +12,15 @@ export class GetOrgRevenueByProvinceService {
     @Inject('EventsRepository') private readonly eventsRepository: EventsRepository,
     private readonly getAdminAccessService: GetAdminAccessService,
     private readonly slackService: SlackService,
+    private readonly fileCacheService: FileCacheService,
   ) {}
 
   async execute(email: string): Promise<Result<ProvinceRevenueData[], Error>> {
     try {
+      const cacheData = await this.fileCacheService.getCache('getOrgRevenueByProvince', {}) as ProvinceRevenueData[];
+      if (cacheData && cacheData.length > 0) {
+        return Ok(cacheData);
+      }
       const isAdmin = await this.getAdminAccessService.execute(email);
       if (!isAdmin) return Err(new Error('You do not have permission to get organizer revenue'));
 
@@ -22,6 +28,13 @@ export class GetOrgRevenueByProvinceService {
       if (result.isErr()) {
         return Err(new Error(result.unwrapErr().message));
       }
+
+      await this.fileCacheService.cacheEndpoint(
+        'getOrgRevenueByProvince',
+        60 * 24,
+        {},
+        result.unwrap(),
+      )
 
       return result;
     } catch (error) {

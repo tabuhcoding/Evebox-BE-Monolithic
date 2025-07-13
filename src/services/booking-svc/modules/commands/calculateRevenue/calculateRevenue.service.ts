a6154443513380dbb28906ffced7ffa36133b30d
tabuhcoding <baobao11062003@gmail.com>
2025-07-13 17:56:15 +0700

    calculate revenue service

--- src/services/booking-svc/modules/commands/calculateRevenue/calculateRevenue.service.ts
+++ src/services/booking-svc/modules/commands/calculateRevenue/calculateRevenue.service.ts
@@ -1,8 +1,11 @@
+import { GetTicketTypeDetailService } from 'src/services/event-svc/modules/ticketType/queries/getTicketTypeDetail/getTicketTypeDetail.service';
+import { GetPreviewShowingService } from 'src/services/event-svc/modules/showing/queries/getPreviewShowing/getPreviewShowing.service';
 import { Inject, Injectable } from "@nestjs/common";
 import { SlackService } from "src/infrastructure/adapters/slack/slack.service";
-import { OrderRepository } from "src/services/booking-svc/repository/order/order.repo";
-import { TicketRepository } from "src/services/booking-svc/repository/ticket/ticket.repo";
+import { BookingTicketStatus, Order, OrderRepository } from "src/services/booking-svc/repository/order/order.repo";
+import { Ticket, TicketRepository } from "src/services/booking-svc/repository/ticket/ticket.repo";
 import { format } from 'date-fns';
+import { EventRevenueData, OrganizerRevenueData, RevenueData, ShowingRevenueData, TicketTypeRevenueData } from "./revenue.dto";
 
 @Injectable()
 export class CalculateRevenueService {
@@ -11,6 +14,7 @@ export class CalculateRevenueService {
     @Inject('OrderRepository') private readonly orderRepository: OrderRepository,
     @Inject('TicketRepository') private readonly ticketRepository: TicketRepository,
     private readonly slackService: SlackService,
+    private readonly getPreviewShowingService: GetPreviewShowingService,
   ) {}
 
   async getAllDatesInOrder(): Promise<string[]> {
@@ -23,17 +27,117 @@ export class CalculateRevenueService {
     return uniqueDays;  
   }
 
-  async getRevenueByDate(date: string): Promise<number> {
-    const orders = await this.orderRepository.findAll({
-      where: {
-        createdAt: {
-          gte: new Date(`${date}T00:00:00Z`),
-          lt: new Date(`${date}T23:59:59Z`),
+  async getRevenueByDate(date: string): Promise<RevenueData> {
+    try {
+      const orders = await this.orderRepository.findAll({
+          createdAt: {
+            gte: new Date(`${date}T00:00:00Z`),
+            lt: new Date(`${date}T23:59:59Z`),
+          },
+          status: BookingTicketStatus.SUCCESS,
         },
-      },
-    });
+        {
+          Ticket: true
+        }
+      );
 
-    const totalRevenue = orders.reduce((sum, order) => sum + order.totalPrice, 0);
-    return totalRevenue;
+      // var orderMappingShowingId = new Map<string, Order[]>();
+      // var ticketMappingTicketTypeId = new Map<string, number>();
+
+      var revenueData: RevenueData = {
+        date: new Date(date),
+        total_revenue: 0,
+        organizers: new Map<string, OrganizerRevenueData>()
+      };
+
+      var showingMapping = new Map<string, ShowingRevenueData>();
+      orders.forEach((order) => {
+        // showing
+        if (!showingMapping.has(order.showingId)) {
+          showingMapping.set(order.showingId, {
+            showing_id: order.showingId,
+            start_date: new Date(),
+            end_date: new Date(),
+            total_revenue: 0,
+            ticket_types: new Map<string, TicketTypeRevenueData>()
+          });
+        }
+        // ticket types
+        order.Ticket.forEach((ticket) => {
+          const showing_revenue = showingMapping.get(order.showingId);
+          if (showing_revenue.ticket_types.has(ticket.ticketTypeId)) {
+            const ticketTypeRevenue = showing_revenue.ticket_types.get(ticket.ticketTypeId);
+            ticketTypeRevenue.sold += 1;
+            showing_revenue.ticket_types.set(ticket.ticketTypeId, ticketTypeRevenue);
+          } else {
+            showing_revenue.ticket_types.set(ticket.ticketTypeId, {
+              name: "",
+              price: 0,
+              sold: 1,
+              total_revenue: 0
+            });
+          }
+        });
+      });
+
+      for (const [showingId, showingData] of showingMapping.entries()) {
+        const showingDetail = await this.getPreviewShowingService.execute(showingId);
+        if (!showingDetail) {
+          continue;
+        }
+        showingData.start_date = showingDetail.startTime;
+        showingData.end_date = showingDetail.endTime;
+
+        // Calculate total revenue for each ticket type
+        showingDetail.TicketType.forEach((ticketType) => {
+          const ticketTypeRevenue = showingData.ticket_types.get(ticketType.id);
+          if (ticketTypeRevenue) {
+            ticketTypeRevenue.name = ticketType.name;
+            ticketTypeRevenue.price = ticketType.price;
+            ticketTypeRevenue.total_revenue = ticketType.price * ticketTypeRevenue.sold;
+            showingData.ticket_types.set(ticketType.id, ticketTypeRevenue);
+          }
+        });
+
+        // Add to organizer data
+        if (!revenueData.organizers.has(showingDetail.orgId)) {
+          revenueData.organizers.set(showingDetail.orgId, {
+            org_id: showingDetail.orgId,
+            org_name: showingDetail.orgId,
+            total_revenue: showingData.total_revenue,
+            events: new Map<number, EventRevenueData>()
+          });
+        }
+
+        const organizerData = revenueData.organizers.get(showingDetail.orgId);
+
+        // Add to event data
+        if (!organizerData.events.has(showingDetail.eventId)) {
+          organizerData.events.set(showingDetail.eventId, {
+            event_id: showingDetail.eventId,
+            event_name: showingDetail.title,
+            total_revenue: showingData.total_revenue,
+            showings: new Map<string, ShowingRevenueData>()
+          });
+        }
+
+        const eventData = organizerData.events.get(showingDetail.eventId);
+        
+        eventData.showings.set(showingId, showingData);
+        eventData.total_revenue += showingData.total_revenue;
+
+        organizerData.total_revenue += showingData.total_revenue;
+        organizerData.events.set(showingDetail.eventId, eventData);
+
+        // Add to revenue data
+        revenueData.total_revenue += showingData.total_revenue;
+        revenueData.organizers.set(showingDetail.orgId, organizerData);
+      }
+
+      return revenueData;
+    } catch (error) {
+      this.slackService.sendError(`Error in CalculateRevenueService.getRevenueByDate: ${error.message}`);
+      return null;
+    }
   }
 }

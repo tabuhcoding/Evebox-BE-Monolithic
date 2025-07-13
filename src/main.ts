1af6288bccdf5f053d6410b9ca9f9fde982e9853
tabuhcoding <baobao11062003@gmail.com>
2025-07-13 18:10:56 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     origin: ['https://evebox.azurewebsites.net', 'http://localhost:3000', 'https://evebox-fe.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-send-key'],
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });

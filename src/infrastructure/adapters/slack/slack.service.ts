453ad9da3a7dd9b812563685d55fb38e02ba0588
tabuhcoding <baobao11062003@gmail.com>
2025-07-16 18:15:47 +0700

    bot telegram

--- src/infrastructure/adapters/slack/slack.service.ts
+++ src/infrastructure/adapters/slack/slack.service.ts
@@ -5,8 +5,47 @@ import axios from 'axios';
 export class SlackService {
   private readonly webhookUrl = process.env.SLACK_WEBHOOK_URL!;
   private readonly isDevelopment = process.env.ENV === 'development';
+  private readonly use_telegram = process.env.USE_TELEGRAM! === 'telegram';
+  private readonly token = process.env.TELEGRAM_BOT_TOKEN!;
+  private readonly chatId = process.env.TELEGRAM_CHAT_ID!;
+
+  private async sendMessage(text: string) {
+    if (this.isDevelopment) {
+      console.log('Telegram log:', text);
+      return;
+    }
+
+    const url = `https://api.telegram.org/bot${this.token}/sendMessage`;
+    try {
+      await axios.post(url, {
+        chat_id: this.chatId,
+        text,
+        parse_mode: 'Markdown',
+      });
+    } catch (err) {
+      console.error('Failed to send Telegram message:', err.message);
+    }
+  }
 
   async sendError(message: string) {
+    if (this.use_telegram) {
+      await this.sendMessage(`🚨 *ERROR*:\n${message}`);
+      return;
+    } else {
+      await this.sendSlackError(message);
+    }
+  }
+
+  async sendNotice(message: string) {
+    if (this.use_telegram) {
+      await this.sendMessage(`📝 *NOTICE*:\n${message}`);
+      return;
+    } else{
+      await this.sendSlackNotice(message);
+    }
+  }
+
+  async sendSlackError(message: string) {
     if (this.isDevelopment) {
       console.log('Console error:', message);
       
@@ -21,7 +60,7 @@ export class SlackService {
     }
   }
 
-  async sendNotice(message: string) {
+  async sendSlackNotice(message: string) {
     if (this.isDevelopment) {
       console.log('Console notice:', message);
       
@@ -35,4 +74,6 @@ export class SlackService {
       return;
     }
   }
+
+  
 }

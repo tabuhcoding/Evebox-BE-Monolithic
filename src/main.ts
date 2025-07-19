d6481256c986a43afef8303f2ff290e90fbeea02
DatTruong0108 <dattantruong.work@gmail.com>
2025-07-20 01:58:00 +0700

    fix: API connect showing to seat map

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const options = new DocumentBuilder()
     .setTitle('Evebox API')
     .setDescription('The Evebox API description')
-    .setVersion('2.9')
+    .setVersion('3.0')
     .addBearerAuth(
       {
          type: 'http',

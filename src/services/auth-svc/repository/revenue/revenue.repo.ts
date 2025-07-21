51ba40d611897fe581dc8e2a515492213275d332
tabuhcoding <baobao11062003@gmail.com>
2025-07-22 02:12:23 +0700

    revenue w prisma

--- src/services/auth-svc/repository/revenue/revenue.repo.ts
+++ src/services/auth-svc/repository/revenue/revenue.repo.ts
@@ -1,3 +1,4 @@
+import { AppRevenueData } from "src/services/event-svc/modules/statistics/queries/getOrgRevenue/getOrgRevenue-response.dto";
 import { BaseAuthRepository } from "../base.repository";
 import { Prisma } from "prisma/client-auth";
 
@@ -20,6 +21,8 @@ export type Revenue = Prisma.RevenueGetPayload<{
 }>;
 
 export interface RevenueRepository extends BaseAuthRepository<Revenue, Prisma.RevenueDelegate> {
+  totalRevenue(from?: string, to?: string): Promise<AppRevenueData>;
+  totalRevenueV2(from?: string, to?: string): Promise<AppRevenueData>
   // getRevenueByOrganizerId(organizerId: string): Promise<OrganizeRevenue | null>;
   // getRevenueByEventId(eventId: number): Promise<OrganizeRevenue | null>;
   // getRevenueByShowingId(showingId: string): Promise<OrganizeRevenue | null>;
